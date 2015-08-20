5fa840f87f13c1a10420c1349072f7a62f355022
Dmitry <stormbringer-s@mail.ru>
2015-08-20 11:00:09 +0400

    Create NullToVisibilityConverter.cs

---
+++ NullToVisibilityConverter.cs
@@ -0,0 +1,12 @@
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
