558d87123cb81f876802db639dbbabc3ac7b08d6
Dmitry <stormbringer-s@mail.ru>
2015-08-26 08:55:25 +0400

    Create StringNullToVisibilityConverter.cs

---
+++ StringNullToVisibilityConverter.cs
@@ -0,0 +1,12 @@
+    class StringNullToVisibilityConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
