bbeda40527d2f0d190f9f7e7e6c2cade7bb52b42
Dmitry <stormbringer-s@mail.ru>
2016-11-11 13:11:21 +0400

    Update NullToVisibilityConverter.cs

--- NullToVisibilityConverter.cs
+++ NullToVisibilityConverter.cs
@@ -1,12 +1,19 @@
-    public class NullToVisibilityConverter : IValueConverter
+  class NullToVisibilityConverter:IValueConverter
     {
+        public NullToVisibilityConverter()
+        {
+            ForNullValue = Visibility.Collapsed;
+            ForNotNullValue = Visibility.Visible;
+        }
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? Visibility.Collapsed : Visibility.Visible;
+            return value == null ? ForNullValue : ForNotNullValue;
         }
 
+        public Visibility ForNullValue { get; set; }
+        public Visibility ForNotNullValue { get; set; }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotSupportedException();
+            throw new NotImplementedException();
         }
     }
